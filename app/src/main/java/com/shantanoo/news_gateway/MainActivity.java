8e551398b836eeb1c21cfc1fdd696782e45725ee
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2020-11-17 00:39:03 -0600

    Init

---
+++ app/src/main/java/com/shantanoo/news_gateway/MainActivity.java
@@ -0,0 +1,14 @@
+package com.shantanoo.news_gateway;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
