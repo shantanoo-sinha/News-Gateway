26ef6281e3aff0ad58e0f9dd0b817440555848e0
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2020-11-18 22:53:25 -0600

    Added API Key

--- app/src/main/java/com/shantanoo/news_gateway/MainActivity.java
+++ app/src/main/java/com/shantanoo/news_gateway/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String API_KEY = "5b944cc9c74f46bfae60f39673108944";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
