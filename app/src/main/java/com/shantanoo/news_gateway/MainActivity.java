c8ea0228c518d5aac70e0a8b912057c68e561bd7
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2020-11-24 00:52:53 -0600

    Added menu

--- app/src/main/java/com/shantanoo/news_gateway/MainActivity.java
+++ app/src/main/java/com/shantanoo/news_gateway/MainActivity.java
@@ -1,8 +1,11 @@
 package com.shantanoo.news_gateway;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.shantanoo.news_gateway.model.NewsSource;
 
@@ -11,10 +14,31 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
+    private Set<String> newsCategories;
+    private Menu categoryMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.categories, menu);
+        categoryMenu = menu;
+        categoryMenu.add("all");
+        for (String category : newsCategories)
+            categoryMenu.add(category);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 
     public void populateSourceAndCategory(Set<String> newsCategories, List<NewsSource> newsSources) {
